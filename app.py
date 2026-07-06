b8231105f3eee7e0ffda4ba8e31b7d77bd950dcd
Nematillo-2210 <nematilloabduhalikov5@gmail.com>
2026-07-06 17:17:29 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import gradio as gr
+from fastai.vision.all import *
+import pathlib
+import platform
+
+plt = platform.system()
+if plt != 'Windows':
+    pathlib.WindowsPath = pathlib.PosixPath
+
+learner = load_learner('weather_classifier.pkl')
+
+def wrapper(img):
+    img = PILImage.create(img)
+    tuple = learner.predict(img)
+    unpacked = dict(zip(learner.dls.vocab, map(float, tuple[2])))
+    return unpacked
+
+inface = gr.Interface(
+    fn=wrapper,
+    inputs=gr.Image(),
+    outputs=gr.Label(),
+    title='Weather Classifier'
+),
+
+inface.launch()
